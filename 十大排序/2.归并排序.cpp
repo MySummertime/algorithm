c00910ab50a9fbce6addb091a863f2d53436c1b2
frances.jiang <frances.bh.jiang@gmail.com>
2023-01-02 17:48:33 +0800

    add a in-place recursion solution to 2.归并排序.cpp

--- 十大排序/2.归并排序.cpp
+++ 十大排序/2.归并排序.cpp
@@ -3,6 +3,13 @@
 using std::vector;
 
 
+void swap(vector<int>& nums, int i, int j) {
+    nums[i] = nums[i] + nums[j];
+    nums[j] = nums[i] - nums[j];
+    nums[i] = nums[i] - nums[j];
+}
+
+
 /*1.自顶向下递归--非原地
 Time: O(nlogN)
 Space: O(n)
@@ -10,19 +17,19 @@ Space: O(n)
 vector<int> mergeSort1(vector<int> nums) {
     if (nums.size() < 2)    return nums;
     vector<int> vec(nums.size());
-    sort(nums, vec, 0, nums.size() - 1);
+    sort1(nums, vec, 0, nums.size() - 1);
     return nums;
 }
 
-void sort(vector<int>& nums, vector<int>& vec, int left, int right) {
+void sort1(vector<int>& nums, vector<int>& vec, int left, int right) {
     if (left >= right)  return;
     int mid = left + ((right - left) >> 1);
-    sort(nums, vec, left, mid);
-    sort(nums, vec, mid + 1, right);
-    merge(nums, vec, left, mid, right);
+    sort1(nums, vec, left, mid);
+    sort1(nums, vec, mid + 1, right);
+    merge1(nums, vec, left, mid, right);
 }
 
-void merge(vector<int>& nums, vector<int>& vec, int left, int mid, int right) {
+void merge1(vector<int>& nums, vector<int>& vec, int left, int mid, int right) {
     int l = left, r = mid + 1;
     int idx = left;
     while (l <= mid && r <= right) {
@@ -47,5 +54,36 @@ Space: O(logN)
 */
 vector<int> mergeSort2(vector<int>& nums) {
     if (nums.size() < 2)    return nums;
-    
+    sort2(nums, 0, nums.size() - 1);
+    return nums;
+}
+
+void sort2(vector<int>& nums, int left, int right) {
+    if (left >= right)  return;
+    int mid = left + ((right - left) >> 1);
+    sort2(nums, left, mid);
+    sort2(nums, mid + 1, right);
+    blockSwap(nums, left, mid, right);
+}
+
+void blockSwap(vector<int>& nums, int left, int mid, int right) {
+    int l = left, r = mid + 1;
+    while (l < r && r <= right) {
+        while (l < r && nums[l] <= nums[r]) ++l;
+        int idx = r;
+        while (r <= right && nums[r] < nums[l]) ++r;
+        exchange(nums, l, idx - 1, r - 1);
+    }
+}
+
+void exchange(vector<int>& nums, int l, int idx, int r) {
+    reverse(nums, l, idx);
+    reverse(nums, idx + 1, r);
+    reverse(nums, l, r);
+}
+
+void reverse(vector<int>& nums, int l, int r) {
+    while (l < r) {
+        swap(nums, l++, r--);
+    }
 }
