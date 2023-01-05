c162eacc13c7e9b4043fc860b6febda6b04083cd
frances.jiang <frances.bh.jiang@gmail.com>
2023-01-05 21:33:08 +0800

    add a new solution to 70.Climbing Stairs.cpp

--- 动态规划/基础/1.爬楼梯.cpp
+++ 动态规划/基础/1.爬楼梯.cpp
@@ -39,3 +39,21 @@ public:
         return dp[2];
     }
 };
+
+
+class Solution3 {
+public:
+    // dynamic programming(完全背包)
+    // Time: O(m*n)
+    // Space: O(n)
+    int climbStairs(int n, int m = 2) {
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (i - j >= 0) dp[i] += dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+};
