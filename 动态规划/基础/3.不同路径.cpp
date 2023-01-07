7eff05d6a9d8bdf182323857a212e8a289217e0a
frances.jiang <frances.bh.jiang@gmail.com>
2023-01-07 19:23:58 +0800

    add unoptimized dp solution to 62

--- 动态规划/基础/3.不同路径.cpp
+++ 动态规划/基础/3.不同路径.cpp
@@ -53,6 +53,25 @@ class Solution3 {
 public:
     // dynamic programming
     // Time: O(m*n)
+    // Space: O(m*n)
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; ++i) dp[i][0] = 1;
+        for (int j = 0; j < n; ++j) dp[0][j] = 1;
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+
+
+class Solution4 {
+public:
+    // dynamic programming
+    // Time: O(m*n)
     // Space: O(n)
     int uniquePaths(int m, int n) {
         vector<int> dp(n, 1);
